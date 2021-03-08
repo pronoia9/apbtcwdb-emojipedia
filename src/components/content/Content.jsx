2022a395feaeb0bcda1b66e048e5d08fa03bcf3e
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-08 18:01:36 +0300

    add alternate return function, tbd

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -4,6 +4,19 @@ import emojipedia from '../../emojipedia';
 
 function Content() {
   return <dl className='dictionary'>{emojipedia.map(createEntry)}</dl>;
+  // return (
+  //   <dl className='dictionary'>
+  //     {emojipedia.map((emoji) => (
+  //       <Entry
+  //         id={emoji.id}
+  //         key={emoji.id}
+  //         emoji={emoji.emoji}
+  //         name={emoji.name}
+  //         meaning={emoji.meaning}
+  //       />
+  //     ))}
+  //   </dl>
+  // );
 }
 
 function createEntry(emoji) {
