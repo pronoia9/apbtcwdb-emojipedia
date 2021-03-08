e99126c9a52a1cbdde7b90f37ff9dbf4e0cecfca
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-08 18:06:13 +0300

    change return function for content

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -3,31 +3,18 @@ import Entry from './Entry';
 import emojipedia from '../../emojipedia';
 
 function Content() {
-  return <dl className='dictionary'>{emojipedia.map(createEntry)}</dl>;
-  // return (
-  //   <dl className='dictionary'>
-  //     {emojipedia.map((emoji) => (
-  //       <Entry
-  //         id={emoji.id}
-  //         key={emoji.id}
-  //         emoji={emoji.emoji}
-  //         name={emoji.name}
-  //         meaning={emoji.meaning}
-  //       />
-  //     ))}
-  //   </dl>
-  // );
-}
-
-function createEntry(emoji) {
   return (
-    <Entry
-      id={emoji.id}
-      key={emoji.id}
-      emoji={emoji.emoji}
-      name={emoji.name}
-      meaning={emoji.meaning}
-    />
+    <dl className='dictionary'>
+      {emojipedia.map((emoji) => (
+        <Entry
+          id={emoji.id}
+          key={emoji.id}
+          emoji={emoji.emoji}
+          name={emoji.name}
+          meaning={emoji.meaning}
+        />
+      ))}
+    </dl>
   );
 }
 
